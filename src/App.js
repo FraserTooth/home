f526037c18b376e643ef3301ef9191f4a2d6b1e9
Fraser <frasertooth@gmail.com>
2019-08-07 16:43:43 +0900

    Revert "Revert "Test""
    This reverts commit 5f53cb5cf4dd37e8b3a1a9765ebd75419483be6e.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Testing
         </a>
       </header>
     </div>
