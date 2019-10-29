e488c45094447dd0db7a835dfe0f1c2c0e39f3ce
Fraser Tooth <frasertooth@gmail.com>
2019-10-29 17:47:44 +0900

    Rebuilt as Next.js app

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <div>
+        <p>Fraser's Next.js About Page</p>
+      </div>
+    );
+  }
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+    <div>
+      <p>Fraser's Next.js Homepage</p>
+    </div>
+  )
+  
+  export default Index
